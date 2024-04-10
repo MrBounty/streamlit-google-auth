2b6ded883acb8725e0a37efad1642ccbaacbff91
Adrien <adrien.bouvais.pro@gmail.com>
2024-04-10 10:47:06 +0200

    Updated examples

--- example1.py
+++ example1.py
@@ -3,24 +3,22 @@ from streamlit_google_auth import Authenticate
 
 st.title('Streamlit Google Auth Example 1')
 
-if 'connected' not in st.session_state:
-    authenticator = Authenticate(
-        secret_credentials_path = 'google_credentials.json',
-        cookie_name='my_cookie_name',
-        cookie_key='this_is_secret',
-        redirect_uri = 'http://localhost:8501',
-    )
-    st.session_state["authenticator"] = authenticator
+authenticator = Authenticate(
+    secret_credentials_path = 'google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri = 'http://localhost:8501',
+)
 
 # Catch the login event
-st.session_state["authenticator"].check_authentification()
+authenticator.check_authentification()
 
 # Create the login button
-st.session_state["authenticator"].login()
+authenticator.login()
 
 if st.session_state['connected']:
     st.image(st.session_state['user_info'].get('picture'))
     st.write('Hello, '+ st.session_state['user_info'].get('name'))
     st.write('Your email is '+ st.session_state['user_info'].get('email'))
     if st.button('Log out'):
-        st.session_state["authenticator"].logout()
+        authenticator.logout()
--- example2.py
+++ example2.py
@@ -1,17 +1,15 @@
 import streamlit as st
 from streamlit_google_auth import Authenticate
 
-if 'connected' not in st.session_state:
-    authenticator = Authenticate(
-        secret_credentials_path = 'google_credentials.json',
-        cookie_name='my_cookie_name',
-        cookie_key='this_is_secret',
-        redirect_uri = 'http://localhost:8501',
-    )
-    st.session_state["authenticator"] = authenticator
+authenticator = Authenticate(
+    secret_credentials_path = 'google_credentials.json',
+    cookie_name='my_cookie_name',
+    cookie_key='this_is_secret',
+    redirect_uri = 'http://localhost:8501',
+)
 
 # Catch the login event
-st.session_state["authenticator"].check_authentification()
+authenticator.check_authentification()
 
 st.title('Streamlit Google Auth Example 2')
 
@@ -20,9 +18,9 @@ if st.session_state['connected']:
     st.write('Hello, '+ st.session_state['user_info'].get('name'))
     st.write('Your email is '+ st.session_state['user_info'].get('email'))
     if st.button('Log out'):
-        st.session_state["authenticator"].logout()
+        authenticator.logout()
 else:
     st.write('You are not connected')
-    authorization_url = st.session_state["authenticator"].get_authorization_url()
+    authorization_url = authenticator.get_authorization_url()
     st.markdown(f'[Login]({authorization_url})')
     st.link_button('Login', authorization_url)
